17c029e886c1cde2302de42bd9a65e40366063bf
melvinrabang <mirabang_88@yahoo.com>
2017-10-22 06:32:57 +0800

    First commit

---
+++ projectapi/src/main/java/com/example/demo/ProjectapiApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectapiApplication.class, args);
+	}
+}
